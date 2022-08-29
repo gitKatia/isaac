2306b2405b9707d557622df815bc326a609ad63f
gitKatia <katia.iacomussi@libero.it>
2022-08-29 13:01:53 +0100

    Add test for flight path angle

--- src/main/java/com/kat/isaac/model/MeasurementUnit.java
+++ src/main/java/com/kat/isaac/model/MeasurementUnit.java
@@ -5,6 +5,7 @@ public final class MeasurementUnit {
     private MeasurementUnit() {
     }
 
-    public static Double FT = 12 * 2.54 / 100;
-    public static Double NM = 1852d;
+    public static Double FT_IN_METERS = 12 * 2.54 / 100;
+    public static Double NM_IN_METERS = 1852d;
+    public static Double RAD_IN_DEG = 180 / Math.PI;
 }
--- src/test/java/com/kat/isaac/service/NewtonOrbitCalculatorTest.java
+++ src/test/java/com/kat/isaac/service/NewtonOrbitCalculatorTest.java
@@ -9,7 +9,8 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
-import static com.kat.isaac.model.MeasurementUnit.FT;
+import static com.kat.isaac.model.MeasurementUnit.FT_IN_METERS;
+import static com.kat.isaac.model.MeasurementUnit.RAD_IN_DEG;
 import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -45,7 +46,7 @@ class NewtonOrbitCalculatorTest {
                 .z(10.463)
                 .build()
                 .multiply(radiusMultiplier)
-                .multiply(FT);
+                .multiply(FT_IN_METERS);
 
         double velocityMultiplier = Math.pow(10, 4);
         Vector v = Vector.builder()
@@ -53,7 +54,7 @@ class NewtonOrbitCalculatorTest {
                 .y(5.1872)
                 .build()
                 .multiply(velocityMultiplier)
-                .multiply(FT);
+                .multiply(FT_IN_METERS);
 
         double result = newtonOrbitCalculator.specificMechanicalEnergy(r, v, Planet.EARTH);
         DecimalFormat df = new DecimalFormat("#.##");
@@ -74,7 +75,7 @@ class NewtonOrbitCalculatorTest {
                 .z(10.463)
                 .build()
                 .multiply(radiusMultiplier)
-                .multiply(FT);
+                .multiply(FT_IN_METERS);
 
         double velocityMultiplier = Math.pow(10, 4);
         Vector v = Vector.builder()
@@ -82,7 +83,7 @@ class NewtonOrbitCalculatorTest {
                 .y(5.1872)
                 .build()
                 .multiply(velocityMultiplier)
-                .multiply(FT);
+                .multiply(FT_IN_METERS);
 
         // When
         double result = newtonOrbitCalculator.specificAngularMomentum(r, v);
@@ -91,5 +92,37 @@ class NewtonOrbitCalculatorTest {
         df.setRoundingMode(RoundingMode.HALF_UP);
         char decimalFormatSeparator = DecimalFormatSymbols.getInstance().getDecimalSeparator();
         assertEquals(format("566087291925%s2", decimalFormatSeparator), df.format(result));
+    }
+
+    @DisplayName("flightPathAngle()")
+    @Test
+    void flightPathAngle() {
+
+        // Given
+        double radiusMultiplier = Math.pow(10, 7);
+        Vector r = Vector.builder()
+                .x(4.1852)
+                .y(6.2778)
+                .z(10.463)
+                .build()
+                .multiply(radiusMultiplier)
+                .multiply(FT_IN_METERS);
+
+        double velocityMultiplier = Math.pow(10, 4);
+        Vector v = Vector.builder()
+                .x(2.5963)
+                .y(5.1872)
+                .build()
+                .multiply(velocityMultiplier)
+                .multiply(FT_IN_METERS);
+
+        // When
+        double result = newtonOrbitCalculator.flightPathAngle(r, v);
+
+        // Then
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        char decimalFormatSeparator = DecimalFormatSymbols.getInstance().getDecimalSeparator();
+        assertEquals(format("35%s48", decimalFormatSeparator), df.format(result * RAD_IN_DEG));
     }
 }
