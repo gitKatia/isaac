aff7d09fa09eb08dfb1b53cbf3162636ff72c50e
gitKatia <katia.iacomussi@libero.it>
2022-09-04 13:45:48 +0100

    Add escape velocity test

--- src/test/java/com/kat/isaac/service/NewtonVelocityCalculatorTest.java
+++ src/test/java/com/kat/isaac/service/NewtonVelocityCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.kat.isaac.TestHelper.*;
+import static com.kat.isaac.model.MeasurementUnit.NM_IN_METERS;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NewtonVelocityCalculatorTest {
@@ -21,7 +22,23 @@ class NewtonVelocityCalculatorTest {
         Double result = newtonVelocityCalculator.circularOrbitVelocity(42168000, standardGravitationalParameter);
 
         // Then
-        assertTrue(result > 3000);
         assertDouble("3075%s54", result);
+    }
+
+    @DisplayName("escapeVelocity()")
+    @Test
+    void escapeVelocity() {
+
+        // Given
+        Double height = 100 * NM_IN_METERS;
+        double earthMeanEquatorialRadius =  EARTH_MEAN_EQUATORIAL_RADIUS;
+        Double radius = height + earthMeanEquatorialRadius;
+        double standardGravitationalParameter = standardGravitationalParameter(PLANET_EARTH_MASS);
+
+        // When
+        Double result = newtonVelocityCalculator.escapeVelocity(radius, standardGravitationalParameter);
+
+        // Then
+        assertDouble("11024%s66", result);
     }
 }
